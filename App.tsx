53d1ed1697ed813b5e09f72e3cc98ae6d5fc9cc2
Marlon Bailey Jr <108299124+Sweatlys@users.noreply.github.com>
2023-02-23 18:20:17 -0500

    error

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { NavigationContainer,  } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Pressable } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import ChatScreen from './screens/ChatScreen';
 import RegisterScreen from './screens/Register';
 import CreateRoom from './screens/CreateRoom';
 import RoomLogin from './screens/RoomLoginScreen'
+import { FontAwesome } from '@expo/vector-icons';
 
 import React from 'react';
 
@@ -21,13 +22,20 @@ export default function App() {
          <Stack.Screen options={{headerShown: false }} name="Login" component={LoginScreen} />
          <Stack.Screen name="ChatScreen" component={ChatScreen} options={{
           headerRight: () => (
-            <Button
+            <Pressable
               onPress={() => navigator.navigate('ChatModal')}
-              title="Info"
-              color="#fff"
-            />
-          ),
-        }}/>
+              style={({ pressed }) => ({
+                opacity: pressed ? 0.5 : 1,
+              })}>
+                <FontAwesome 
+                  name="info-circle"
+                  size={24}
+                  color="black"
+                  style={{marginRight: 15}}
+                  />
+            </Pressable>
+          )
+            }} />
          <Stack.Screen name="Register" component={RegisterScreen} />
          <Stack.Screen name="CreateRoom" component={CreateRoom} />
          <Stack.Screen name="RoomLogin" component={RoomLogin} />
